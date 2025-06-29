e3caa0bc5b740bba5619fc55f88930c8efe98c95
1 <1>
2025-06-29 15:49:52 +0800

    C语言学习9

---
+++ C语言学习9/kecheng9/test.c
@@ -0,0 +1,23 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//void swap(int* px, int* py)
+//{
+//	int tmp = *px;
+//	*px = *py;
+//	*py = tmp;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	if (a < b)
+//		swap(&a, &b);
+//	if (a < c)
+//		swap(&a, &c);
+//	if (b < c)
+//		swap(&b, &c);
+//	printf("%d %d %d", a, b, c);
+//}
