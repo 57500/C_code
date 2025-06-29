24ac46731bbe2dc73af8b87eaa7e50ea805f6399
1 <1>
2025-06-29 15:22:58 +0800

    C语言学习3

---
+++ C语言学习3/kecheng3/test.c
@@ -0,0 +1,36 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//		printf("%d\n", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//struct Stu
+//{
+//	char name[10];
+//	int age;
+//	char sex[10];
+//	char tele[12];
+//
+//};
+//
+//int main()
+//{
+//	struct Stu s = { "zhangsan",20,"nan","14754800270" };
+//	printf("%s %d %s %s\n", s.name, s.age, s.sex, s.tele);
+//	return 0;
+//}
